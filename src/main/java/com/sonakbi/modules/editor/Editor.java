7bf7f06cc2a82c025821680a1db7d2648211abc2
임태희 <imth1129@gmail.com>
2024-07-24 16:35:59 +0900

    포스팅 ui 작업

--- src/main/java/com/sonakbi/modules/editor/Editor.java
+++ src/main/java/com/sonakbi/modules/editor/Editor.java
@@ -1,8 +1,10 @@
 package com.sonakbi.modules.editor;
 
+import com.sonakbi.modules.account.Account;
 import com.sonakbi.modules.series.Series;
 import com.sonakbi.modules.tag.Tag;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,9 +20,19 @@ public class Editor {
     @Id @GeneratedValue
     private Long id;
 
+    @Size(min = 1, max = 100)
+    @Column(nullable = false)
     private String title;
 
+    @Lob @Basic(fetch = FetchType.EAGER)
+    @Column(nullable = false)
     private String mainText;
+
+    @OneToOne
+    private Account writer;
+
+    @Column(nullable = false)
+    private String url;
 
     @ManyToMany
     private Set<Tag> tags;
